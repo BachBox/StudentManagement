eaf6503a3aca488bef433401099ef7622d11a825
BachBox <101316321+BachBox@users.noreply.github.com>
2022-10-22 18:57:56 +0700

    Add files via upload

---
+++ StudentManagement/Main.java
@@ -0,0 +1,20 @@
+package StudentManagement;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Class classes = new Class();
+        System.out.println("How many student u wanna input: ");
+        int n = Integer.parseInt(sc.nextLine());
+        classes.inputStudents(sc, n);
+        classes.printStudents();
+        System.out.println("---------------------");
+        classes.printSpecialStudents(1985, "thai nguyen");
+        System.out.println("--------------------");
+        classes.printStudentsInAClass("10s1");
+
+    }
+}
